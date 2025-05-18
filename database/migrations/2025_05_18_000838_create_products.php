c66f0da86fd663997af16e84c4ccf7a6f0491b82
AchmadFadill <ahmadfadillah12222@gmail.com>
2025-05-18 07:12:37 +0700

    membuat migration

---
+++ database/migrations/2025_05_18_000838_create_products.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('code')->unique();
+            $table->string('barcode')->nullable()->unique();
+            $table->text('description')->nullable();
+            $table->foreignId('product_category_id')->nullable()->constrained()->nullOnDelete();
+            $table->decimal('purchase_price', 15, 2)->default(0);
+            $table->decimal('selling_price', 15, 2)->default(0);
+            $table->integer('alert_quantity')->default(0);
+            $table->string('unit')->nullable(); // pcs, kg, liter, dll.
+            $table->string('image')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
